4a43e4371232aa7e0f432223d66a01aefeef2259
Gag7117 <123760741+Gag7117@users.noreply.github.com>
2023-01-29 20:00:29 +0200

    Add files via upload

---
+++ BoilerBasedOnHoursOfSun_v1.0.js
@@ -0,0 +1,57 @@
+let CONFIG = {
+  accuWeatherAPIKEY: "54YhDCrUiABBscPtmVbzfTNGUcppnYte",
+  weatherForecastEndpoint:
+    "http://dataservice.accuweather.com/forecasts/v1/daily/1day/",
+  weatherCurrentEndpoint:
+    "http://dataservice.accuweather.com/currentconditions/v1/",
+  locations: {
+    Timisoara: 290867,
+    Sofia: 51097,
+  },
+  //check every 24h
+  checkInterval: 1000 * 60 * 60 * 24, // check weather forecast once every 24h (value in ms)
+  boilerSwitchThreshold: 1, //HoursOfSun value in hours, for determining whether to activate the electric boiler
+};
+
+function getWeatherURLForLocation(location) {
+  return (
+    CONFIG.weatherForecastEndpoint +
+    JSON.stringify(CONFIG.locations[location]) +
+    "?apikey=" +
+    CONFIG.accuWeatherAPIKEY +
+    "&details=true"
+  );
+}
+
+function activateSwitch(activate) {
+  Shelly.call(
+    "Switch.Set",
+    { id: 0, on: activate },
+    function (response, error_code, error_message) {}
+  );
+}
+
+function callback(location) {
+  Shelly.call(
+    "http.get",
+    { url: getWeatherURLForLocation(location) },
+    function (response, error_code, error_message, location) {
+      let weatherData = JSON.parse(response.body);
+      let SunTime = weatherData.DailyForecasts[0].HoursOfSun;
+      print("Expected hours of Sun for tomorrow:", SunTime);
+      if (SunTime <= CONFIG.boilerSwitchThreshold) {
+        activateSwitch(true);
+        print("Electric water heating ON")
+      }
+      if (SunTime > CONFIG.boilerSwitchThreshold) {
+        activateSwitch(false);
+        print("Free hot water!")
+      }
+    },
+    location
+  );
+}
+
+callback("Timisoara");
+// Set up a regular interval - 24h - to check the expected HoursOfSun for next day
+Timer.set(CONFIG.checkInterval, true, callback, "Timisoara");
